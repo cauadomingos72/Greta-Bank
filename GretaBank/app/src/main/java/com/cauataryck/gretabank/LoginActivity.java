7c7051ce79bf3f47113ea0c6119cb815880d1cda
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-10-20 10:22:03 -0300

    Add files via upload

---
+++ GretaBank/app/src/main/java/com/cauataryck/gretabank/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.cauataryck.gretabank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login_btn;
+    private TextView signupRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login_btn = findViewById(R.id.login_btn);
+        TextView signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
